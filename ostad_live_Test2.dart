7478a4b84dc17c4b4bc86cc8165a677f9795a2ef
Md Shoun babu Prince <shounbabu31@gmail.com>
2024-11-15 23:02:31 +0600

    Update ostad_live_Test2.dart

--- ostad_live_Test2.dart
+++ ostad_live_Test2.dart
@@ -6,18 +6,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Need Blood'),
-          titleSpacing: 0,
+          title: Text(
+            'Need Blood',
+            style: TextStyle(color: Colors.white),
+          ),
           centerTitle: true,
           backgroundColor: Colors.red,
           actions: [
             IconButton(
               icon: Icon(Icons.add),
               onPressed: () {
-                // Add action here
               },
             ),
           ],
